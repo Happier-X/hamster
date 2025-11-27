d2fd1444920523a39ae76d009575190e9a239c4d
赵鸿飞 <zhf5256@qq.com>
2025-11-27 14:03:26 +0800

    feat: 引入书签分类

--- src/background/messages/bookmarks.ts
+++ src/background/messages/bookmarks.ts
@@ -1,5 +1,8 @@
 import type { PlasmoMessaging } from "@plasmohq/messaging"
 
+import createCategory from "~models/createCategory"
+import parseTreeToMarkdown from "~utils/parse"
+
 const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
   const { action } = req.body
   switch (action) {
@@ -8,6 +11,11 @@ const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
       res.send(bookmarks)
       break
     case "organizeBookmarks":
+      const bookmarksTree = await chrome.bookmarks.getTree()
+      const markdown = parseTreeToMarkdown(bookmarksTree)
+      console.log(1111111, markdown)
+      const response = await createCategory(markdown)
+      console.log(response)
       res.send({ message: "书签整理完成" })
       break
   }
