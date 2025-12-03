3a83fb2a3d932b95a2199a005c73d9a5bd76c97a
赵鸿飞 <zhf5256@qq.com>
2025-12-03 15:20:16 +0800

    feat: 完成时钟、搜索框、设置按钮

--- src/main/App.vue
+++ src/main/App.vue
@@ -1,39 +1,69 @@
-<script setup lang="ts">
-// import HelloWorld from '@/components/HelloWorld.vue'
-</script>
-
 <template>
-  <div style="width: 100vw;height: 100vh;background-color: pink;">
-    <a href="https://vite.dev" target="_blank">
-      <img src="@/assets/vite.svg" class="logo" alt="Vite logo">
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="@/assets/vue.svg" class="logo vue" alt="Vue logo">
-    </a>
-    <a href="https://crxjs.dev/vite-plugin" target="_blank">
-      <img src="@/assets/crx.svg" class="logo crx" alt="crx logo">
-    </a>
+  <div class="w-screen h-screen bg-background flex flex-col items-center relative">
+    <Button variant="ghost" size="icon" class="absolute top-4 right-4">
+      <SettingsIcon />
+    </Button>
+    <div className="text-4xl font-light text-foreground font-mono pb-4 pt-[15vh]">
+      {{ timeString }}
+    </div>
+    <InputGroup class="w-2/5">
+      <InputGroupInput v-model="query" @keyup.enter="handleSearch" />
+      <InputGroupAddon>
+        <span class="icon-[cib--bing]" role="img" aria-hidden="true" v-show="false" />
+        <span class="icon-[cib--google]" role="img" aria-hidden="true" />
+        <span class="icon-[cib--baidu]" role="img" aria-hidden="true" v-show="false" />
+      </InputGroupAddon>
+      <InputGroupAddon align="inline-end">
+        <InputGroupButton size="icon-xs" @click="handleSearch">
+          <SearchIcon />
+        </InputGroupButton>
+      </InputGroupAddon>
+    </InputGroup>
   </div>
-  <!-- <HelloWorld msg="Vite + Vue + CRXJS" /> -->
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup lang="ts">
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+} from '@/components/ui/input-group'
+import { Button } from '@/components/ui/button';
+import { Search as SearchIcon, Settings as SettingsIcon } from 'lucide-vue-next';
+import { ref, onMounted, onUnmounted } from 'vue';
+
+// 时间字符串
+const timeString = ref('');
+// 更新时间
+const updateTime = () => {
+  const now = new Date()
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false
+  }
+  timeString.value = now.toLocaleTimeString("zh-CN", timeOptions)
 }
 
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
+let timer: ReturnType<typeof setInterval> | null = null
 
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+onMounted(() => {
+  updateTime()
+  timer = setInterval(updateTime, 1000)
+})
 
-.logo.crx:hover {
-  filter: drop-shadow(0 0 2em #f2bae4aa);
+onUnmounted(() => {
+  if (timer) {
+    clearInterval(timer)
+  }
+})
+
+// 查询字符串
+const query = ref('')
+// 处理搜索
+const handleSearch = () => {
+  window.open(`https://www.google.com/search?q=${query.value}`)
+  query.value = ""
 }
-</style>
+</script>
